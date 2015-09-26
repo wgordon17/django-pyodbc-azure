1c79f5a51af1a542d6d0a9959c1d7c360d81ca9b
Michiya Takahashi <takahashi.michiya@gmail.com>
2015-09-26 11:45:24 +0900

    Merge pull request #40 from daybarr/check_constraints_speedup
    Speed up check_constraints for empty table list
    Conflicts:
    	sql_server/pyodbc/base.py

--- sql_server/pyodbc/base.py
+++ sql_server/pyodbc/base.py
@@ -329,7 +329,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
 
     def _execute_foreach(self, sql, table_names=None):
         cursor = self.cursor()
-        if not table_names:
+        if table_names is None:
             table_names = self.introspection.get_table_list(cursor)
         for table_name in table_names:
             cursor.execute(sql % self.ops.quote_name(table_name))
